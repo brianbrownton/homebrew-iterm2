98737ad478df7b0f4f9a0c218c37483770146d99
Brian Morton <brianmortondesign@gmail.com>
2016-06-16 16:39:23 -0400

    Update imgls.rb

--- imgls.rb
+++ imgls.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Imgcat < Formula
+class Imgls < Formula
   homepage 'https://www.iterm2.com/images.html'
   url 'https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgls'
 
