493e0458f8a77ecba4f4ea84098f87a836686be1
Brian Morton <brianmortondesign@gmail.com>
2016-06-16 16:44:03 -0400

    Update imgls.rb

--- imgls.rb
+++ imgls.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Imgls < Formula
   homepage 'https://www.iterm2.com/images.html'
   url 'https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgls'
+  sha256 '180d248be0d82f617eb513138448389b6898a6227c09dc0be987ae1819e201f2'
   version '1.0'
 
   def install
